141e341c3d7b1bf236f97499e490169d274f0d7f
Adam Flickema <aflickem@emich.edu>
2016-10-19 11:12:30 -0700

    First Commit

---
+++ security.sh
@@ -0,0 +1,225 @@
+#!/bin/bash
+
+#######################
+#Author: Adam Flickema#
+#############################
+#Contact: aflickem@emich.edu#
+#############################
+
+###############################################
+#Only meant for OSTYPE=linux-gnu at the moment
+#MUST USE SUDO OR RUN AS ROOT
+#Must have apt-get or yum installed
+###############################################
+
+#A linux security script that uses linux best practices and asks the user which things that they would like to use
+#It will be able to be used on different forms on linux ostypes
+
+#Version 2
+#Added support for Ubuntu/Debian systems
+
+apt=`command -v apt-get`
+yum=`command -v yum`
+
+
+if [ ! -z "$apt" ]; then
+	apt-get install lsb-release &> /dev/null
+elif [ ! -z "$yum" ]; then
+	yum install lsb-release &> /dev/null
+else
+	echo "No supported pkg manager."
+	exit 1;
+fi
+
+os=`lsb_release -si`
+
+echo '################'
+echo "Linux Security"
+echo '################'
+echo ""
+echo '###########################'
+echo "Operating System: $os"
+echo '###########################'
+echo ""
+#Tests operating system ID and if CentOS runs all commands for CentOS
+if [ "$os" == "CentOS" ] || [ "$os" == "Red Hat" ]; then
+	
+	echo '######################'
+	echo "Checking for Updates"
+	echo '######################'
+	echo ""
+	echo '####################'
+	echo "Available Updates:"
+	echo '####################'
+	
+	yum check-update | awk '(NR >=4) {print $1;}' | sed ':a;N;$!ba;s/\n/,/g'
+	while true; do
+		read -p "Would you like to install this update?" yn
+		case $yn in
+			[Yy]* ) yum update
+				break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+
+	echo ""
+
+#Asks the user if they would like to update the programs or not
+
+	echo '##############'
+	echo "Disable IPV6"
+	echo '##############'
+
+	while true; do
+		if grep -Fxq "alias net-pf-10 off" /etc/modprobe.conf; then
+			echo "IPv6 is already disabled."
+		else
+			read -p "Would you like to disable IPv6?" yn	
+			case $yn in
+				[Yy]* ) sed -i "alias net-pf-10 off" /etc/modprode.conf
+					break;;
+				[Nn]* ) break;;
+				* ) echo "Please answer yes or no.";;
+			esac
+		fi
+	done
+
+
+	echo '####################'
+	echo "Disbale Root Shell"
+	echo '####################'
+
+	while true; do
+		read -p "Would you like to disable root shell?" yn
+		case $yn in
+			[Yy]* ) sed -i -e 's#^root:x:0:0:root:/root:/bin/bash#root:x:0:0:root:/root:/sbin/nologin#' /etc/passwd
+				break;;
+			[Nn]* ) break;;
+			* )  echo "Please answer yes or no.";;
+		esac
+	done
+
+
+	echo '######################'
+	echo "Limit Password Reuse"
+	echo '######################'
+
+	while true; do
+		read -p "Would you like to limit password reuse?" yn
+		case $yn in
+			[Yy]* ) read -p "How many passwords would you like to remember?" num
+				sed -i "17a password    sufficient    pam_unix.so use_authtok md5 shadow remember=$num" /etc/pam.d/system-auth
+				break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+
+
+	echo '####################'
+	echo "Max Logon Attempts"
+	echo '####################'
+
+	while true; do
+		read -p "Would you like have a max logon attempt amount?" yn
+		case $yn in
+			[Yy]* ) read -p "What is the maximum amount of login attempts?" num
+				sed -i "8a auth        required      pam_tally.so no_magic_root" /etc/pam.d/system-auth
+				sed -i "13a account     required      pam_tally.so deny=$num no_magic_root lock_time=180" /etc/pam.d/system-auth
+				break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+
+	echo '########################'
+	echo "Linux Kernel Hardening"
+	echo '########################'
+
+	while true; do
+		read -p "Would you like to turn on execshield?" yn
+		case $yn in
+			[Yy]* ) echo -e "\n#Turn on execshield" >> /etc/sysctl.conf
+				echo "kernel.exec-shield=1" >> /etc/sysctl.conf
+				echo "kernel.randomaize_va_space=1" >> /etc/sysctl.conf
+				break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+
+	while true; do
+		read -p "Would you like to enable IP spoofing protection and logging?" yn
+		case $yn in
+			[Yy]* ) echo -e "\n#Enable IP spoofing protection and logging\nnet.ipv4.conf.all.rp_filter=1\nnet.ipv4.conf.all.log_martians=1" >> /etc/sysctl.conf
+				break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+
+	while true; do
+		read -p "Would you like to disable IP source routing?" yn
+		case $yn in
+			[Yy]* ) echo -e "\n#Disable IP source routing\nnet.ipv4.conf.all.accept_source_route=0" >> /etc/sysctl.conf
+				break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+
+	while true; do
+			read -p "Would you like to block ping and broadcast requests?" yn
+			case $yn in
+                [Yy]* ) echo -e "\n#Ignore broadcast request\nnet.ipv4.icmp_echo_ignore_broadcast=1\nnet.ipv4.icmp_ignore_bogus_error_messages=1" >> /etc/sysctl.conf
+					break;;
+                [Nn]* ) break;;
+                * ) echo "Please answer yes or no.";;
+			esac
+	done
+
+	echo '##################################################'
+	echo "Remove X Windows"
+	echo "!!!Caution!!!"
+	echo "Only do this if you are on a web or mail server."
+	echo '##################################################'
+	while true; do
+        read -p "Would you like to remove X Windows?" yn
+        case $yn in
+			[Yy]* ) yum groupremove "X Windows System"
+				break;;
+            [Nn]* ) break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+	done
+elif [ "$os" == "Ubuntu" ] || [ "$os" == "Debian" ]; then
+	
+	echo '######################'
+	echo "Checking for Updates"
+	echo '######################'
+	echo ""
+	echo '####################'
+	echo "Available Updates:"
+	echo '####################'
+	
+	apt-get -u upgrade --assume-no
+	while true; do
+		read -p "Would you like to install this update?" yn
+		case $yn in
+			[Yy]* ) apt-get update; apt-get upgrade
+				break;;
+			[Nn]* ) break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+	
+	
+	
+	
+else
+	echo "$os is not supported."
+	exit 1;
+fi
+
+exit 0
